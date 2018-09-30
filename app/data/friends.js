07690a54091b55552c3ef79a4c86ab4e9e8de073
Brendon Esperancilla <brendon.esp@gmail.com>
2018-09-30 00:29:40 -0400

    populate friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,70 @@ var friendArray = [
         4,
         1
       ]
+    },
+    {
+      friendName: "Smitty Werbenjagermanjensen",
+      friendPhoto: "https://images.8tracks.com/avatar/i/007/342/353/70756.original-4349.jpg?rect=52,0,265,265&q=98&fm=jpg&fit=max",
+      friendScores: [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+    },
+    {
+      friendName: "Abigail 'Numbuh Five' Lincoln",
+      friendPhoto: "https://vignette.wikia.nocookie.net/knd/images/1/10/Numbuh_5.jpg/revision/latest?cb=20071005005147",
+      friendScores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+      ]
+    },
+    {
+      friendName: "Number 2",
+      friendPhoto: "https://alchetron.com/cdn/number-2-austin-powers-5db2d6bc-7f9c-4412-a6f3-67c12c5e298-resize-750.jpeg",
+      friendScores: [
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2
+      ]
+    },
+    {
+      friendName: "Heather Chandler",
+      friendPhoto: "https://i.pinimg.com/originals/21/8a/74/218a74e0874419ebdbe385b23c033ad1.png",
+      friendScores: [
+        1,
+        2,
+        1,
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        5
+      ]
     }
   ];
 
