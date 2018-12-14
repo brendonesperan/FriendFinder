990eefd0fed7208537b44c205a9db20288066d86
Brendon Esperancilla <brendon.esp@gmail.com>
2018-12-14 15:19:33 -0500

    try routing fix

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 
 app.listen(PORT, function() {
